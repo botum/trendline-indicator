c301dae9c4ca824cad477ee734d07ecf919864dc
Bruno Sarlo <bsarlo@gmail.com>
2018-07-26 01:42:01 -0300

    mucho coding. Mixed all in here, no more branch on-the-fly. Implementing DB here too next.

--- user_data/indicators/trendlines.py
+++ user_data/indicators/trendlines.py
@@ -8,23 +8,19 @@ import copy
 import sys
 from typing import Dict, List, Tuple
 from pandas import DataFrame, to_datetime, Series
+import numpy as np
 
 from datetime import datetime
-
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+# from matplotlib.pyplot import plot, grid, show, savefig
 
 from scipy import linspace, polyval, polyfit, sqrt, stats, randn
 from pylab import plot, title, show , legend
 
-from freqtrade import (DependencyException, OperationalException, exchange, persistence)
-from freqtrade.configuration import Configuration
-# from freqtrade.persistence import Trade, Pair, Trend
-from freqtrade.indicators import in_range
-# import operator
+from technical import indicators
 
-import numpy as np
-import matplotlib.pyplot as plt
 # from matplotlib.ticker import AutoMinorLocator
-import matplotlib.dates as mdates
 
 # ZigZag
 
@@ -32,33 +28,73 @@ import matplotlib.dates as mdates
 import pyximport
 # pyximport.install()
 pyximport.install(reload_support=True)
-from user_data.indicators.zigzag_hi_lo import *
+from user_data.indicators import zigzag_hi_lo
+from zigzag import *
 
-from matplotlib.pyplot import plot, grid, show, savefig
 
-def plot_pivots(X, L, H, pivots):
+def plot_pivots(X, pivots, interval: int, pair: str=None, filename: str=None):
     plt.plot(np.arange(len(X)), X, 'k:', alpha=0.2)
     plt.plot(np.arange(len(X))[pivots != 0], X[pivots != 0], 'k-')
-    plt.scatter(np.arange(len(X))[pivots == 1], H[pivots == 1], color='r', alpha=0.3)
-    plt.scatter(np.arange(len(X))[pivots == -1], L[pivots == -1], color='g', alpha=0.3)
-#     plt.show()
-    pass
+    plt.scatter(np.arange(len(X))[pivots == 1], X[pivots == 1], color='r', alpha=0.3)
+    plt.scatter(np.arange(len(X))[pivots == -1], X[pivots == -1], color='g', alpha=0.3)
 
-def plot_trends(df, interval: int, pivots, filename: str=None):
+    if not filename:
+        filename = 'chart_plots/' + pair.replace('/', '-') + '-' +  interval +  datetime.utcnow().strftime('-%H') + '.png'
+
+    # plt.scatter(df.index[pivots == 1], df.high[pivots == 1], color='r')
+    # plt.scatter(df.index[pivots == -1], df.low[pivots == -1], color='g')
+
+    plt.savefig(filename)
+    plt.close()
+
+# def plot_trends(df, interval: int, pivots, filename: str=None):
+#     plt.figure(num=0, figsize=(20,10))
+#     df['old_date'] = df['date']
+#     to_datetime(df['date'])
+#     df.set_index(['date'],inplace=True)
+#     plt.plot(df.index, df['trendline-max'], 'r', label='resistance trend', linewidth=2)
+#     plt.plot(df.index, df['trendline-min'], 'g', label='support trend', linewidth=2)
+#     plt.plot(df.high, 'r', alpha=0.5)
+#     plt.plot(df.close, 'k', alpha=0.5)
+#     plt.plot(df.low, 'g', alpha=0.5)
+#
+#     plt.plot(df.bb_lowerband, 'b', alpha=0.5, linewidth=2)
+#     plt.plot(df.bb_upperband, 'b', alpha=0.5, linewidth=2)
+#
+#     trends = [col for col in df if col.startswith('trend-')]
+#     for t in trends:
+#         plt.plot(df.index, df[t], 'k', label='trend', alpha=0.5, linewidth=1)
+#
+#     plt.xlim(df.index[0], df.index[-1])
+#     plt.ylim(df.low.min()*0.99, df.high.max()*1.01)
+#     plt.xticks(rotation='vertical')
+#
+#     if not filename:
+#         filename = 'chart_plots/' +  interval + '-' +  datetime.utcnow().strftime('-%S') + '.png'
+#
+#     plt.scatter(df.index[pivots == 1], df.high[pivots == 1], color='r')
+#     plt.scatter(df.index[pivots == -1], df.low[pivots == -1], color='g')
+#
+#     plt.savefig(filename)
+#     plt.close()
+#     df['date'] = df['old_date']
+
+
+def plot_trends(df, interval: int, pair: str=None, filename: str=None):
     plt.figure(num=0, figsize=(20,10))
-    df['old_date'] = df['date']
-    to_datetime(df['date'])
-    df.set_index(['date'],inplace=True)
-    plt.plot(df.index, df['trendline-max'], 'r', label='resistance trend', linewidth=2)
-    plt.plot(df.index, df['trendline-min'], 'g', label='support trend', linewidth=2)
+    # df['old_date'] = df['date']
+    # to_datetime(df['date'])
+    # df.set_index(['date'],inplace=True)
+    plt.scatter(df.index, df['res_trend'], color='r', label='resistance trend', s=10)
+    plt.scatter(df.index, df['sup_trend'], color='g', label='support trend', s=10)
     plt.plot(df.high, 'r', alpha=0.5)
     plt.plot(df.close, 'k', alpha=0.5)
     plt.plot(df.low, 'g', alpha=0.5)
 
-    plt.plot(df.bb_lowerband, 'b', alpha=0.5, linewidth=2)
-    plt.plot(df.bb_upperband, 'b', alpha=0.5, linewidth=2)
+    # plt.plot(df.bb_lowerband, 'b', alpha=0.5, linewidth=2)
+    # plt.plot(df.bb_upperband, 'b', alpha=0.5, linewidth=2)
 
-    trends = [col for col in df if col.startswith('trend-')]
+    trends = [col for col in df if col.startswith('trend|')]
     for t in trends:
         plt.plot(df.index, df[t], 'k', label='trend', alpha=0.5, linewidth=1)
 
@@ -67,14 +103,57 @@ def plot_trends(df, interval: int, pivots, filename: str=None):
     plt.xticks(rotation='vertical')
 
     if not filename:
-        filename = 'chart_plots/' +  interval + '-' +  datetime.utcnow().strftime('-%S') + '.png'
+        filename = 'chart_plots/' + pair.replace('/', '-') + '-' +  interval + \
+                str(len(df)) + datetime.utcnow().strftime('-%H') + '.png'
 
-    plt.scatter(df.index[pivots == 1], df.high[pivots == 1], color='r')
-    plt.scatter(df.index[pivots == -1], df.low[pivots == -1], color='g')
+    # plt.scatter(df.index[pivots == 1], df.high[pivots == 1], color='r')
+    # plt.scatter(df.index[pivots == -1], df.low[pivots == -1], color='g')
 
     plt.savefig(filename)
     plt.close()
-    df['date'] = df['old_date']
+    # df['date'] = df['old_date']
+
+def plot_trends_new(df, interval: int, pair: str=None,  filename: str=None, type: str=None):
+    plt.figure(num=0, figsize=(20,10))
+    # df['old_date'] = df['date']
+    # to_datetime(df['date'])
+    # df.set_index(['date'],inplace=True)
+    # plt.plot(df.index, df['res_trend'], 'r', label='resistance trend', linewidth=2)
+    # plt.plot(df.index, df['sup_trend'], 'g', label='support trend', linewidth=2)
+    plt.plot(df.val, 'k', alpha=0.5)
+    plt.plot(df.index, df['res_trend'], 'r', label='res_trend', linewidth=1)
+    plt.plot(df.index, df['sup_trend'], 'g', label='sup_trend', linewidth=1)
+    trends = [col for col in df if col.startswith('trend|')]
+    for t in trends:
+        plt.plot(df.index, df[t], 'k', label=t, alpha=0.1, linewidth=1)
+        # plt.text(df.index, df[t], t, fontsize=10)
+
+    plt.xlim(df.index[0], df.index[-1])
+    plt.ylim(df.val.min()*0.99, df.val.max()*1.01)
+    plt.xticks(rotation='vertical')
+
+    if not filename:
+        filename = 'chart_plots/' + pair.replace('/', '-') +  interval + '-' + type + '.png'
+
+    plt.scatter(df.loc[pivots == 1].index, df.loc[pivots == 1].high, color='r')
+    plt.scatter(df.loc[pivots == -1].index, df.loc[pivots == -1].low, color='g')
+    # labelLines(plt.gca().get_lines(),zorder=2.5)
+
+    art = []
+    lgd = plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=2)
+    art.append(lgd)
+
+    plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1))
+    plt.savefig(filename, additional_artists=art, bbox_inches="tight")
+    plt.close()
+    # df['date'] = df['old_date']
+
+def in_range(x, target, percent):
+   start = target - target * percent
+   end = target + target * percent
+   check = (start <= x) & (end >= x)
+   # print (check)
+   return check
 
 def get_tests(df, trend_name, pt, first):
 
@@ -91,9 +170,209 @@ def get_tests(df, trend_name, pt, first):
 #     trends['trend'].append(trend)
 #     print(trend_name)
 
-    return trend_tests
+def get_confirmations(df, trend_name, pt, first):
 
-def gentrends(self, df, interval: int, chart=False):
+    trend = df[trend_name]
+
+    if pt == 'res':
+        tolerance = 0.0001
+        t_r = df.loc[(df['pivots']==1) & in_range(df['high'],trend, tolerance)]
+    if pt == 'sup':
+        tolerance = 0.0001
+        t_r = df.loc[(df['pivots']==-1 ) & in_range(df['low'],trend, tolerance)]
+
+    trend_confirmations = len(t_r)
+#     trends['trend'].append(trend)
+#     print(trend_name)
+
+    return trend_confirmations
+
+
+def get_trends_serie(self, serie: Series, interval: int, type: str, tolerance: int, confirmations: int, ticker_gap: int, \
+             fake: int, angle_min: int, angle_max: int, thresh_up: int, thresh_down: int, chart=False, pair: str=None):
+    df = serie.to_frame(name='val')
+    df['trend']= Series()
+    # print (pair)
+    # print (type)
+    # print (len(df))
+    # volatility settings for threshold for peak and valleys
+    volat_window = {
+                '1d':10,
+                '4h':20,
+                '1h':50,
+                '30m':20,
+                '15m':30,
+                '5m':50,
+                '1m':100
+                }
+
+    window = volat_window[interval]
+    # Bollinger bands
+    bollinger = indicators.bollinger_bands(df, field='val', period=window, stdv=1.5)
+
+    # create the BB expansion indicator
+    df['bb_exp'] = (df['bb_upper'] - df['bb_lower']) / df['bb_upper']
+
+    pivots_list = zigzag_hi_lo.peak_valley_pivots(df.val.values, df.val.values, df.bb_exp.values)
+
+    # pivots_list = peak_valley_pivots(df.val.values, thresh_up, thresh_down)
+
+    df['pivots'] = np.transpose(np.array((pivots_list)))
+    # print (df['pivots'])
+    max_confirmations = 0
+
+    # global df
+    # df = list()
+
+    # get only peaks of type we want
+    if type == "res":
+        p = df.loc[df['pivots']==1]
+    if type == "sup":
+        p = df.loc[df['pivots']==-1]
+    # print ('pivots: ', len(p))
+    # print (p)
+    for i in range(0, len(p)-1):
+        ax = p.index[i]
+        ay = p.iloc[i].val
+
+        # trend x axis.
+        t = df.index[ax:]
+
+        # b point is next high
+        # bx = p.index[i+1]
+        # by = p.iloc[i+1].val
+        # t = df.index[ax:]
+
+        next_waves = p[i+1:]
+        # if type == "res":
+        #     cond = by > df.loc[bx][trend_name]
+        #     # id_max = p[:-1].high.values.argmax()
+        # if type == "sup":
+        #     cond = by < df.loc[bx][trend_name]
+
+
+        # check the rest of the highs if they are higher than trend
+        for ib in range(0, len(next_waves)):
+            # print (ib)
+            bx = next_waves.index[ib]
+            by = next_waves.iloc[ib].val
+
+            # trace first trend
+            slope, intercept, r_value, p_value, std_err = stats.linregress([ax, bx], [ay, by])
+            trend = polyval([slope,intercept],t)
+            angle = np.rad2deg(np.arctan2(by - ay, bx - ax))
+            trend_name = 'trend|'+type+'|'+str(ax)+'|'+str(ay)+'|'+str(bx)+'|'+str(by)
+            # print (angle)
+            if angle >= angle_min and angle <= angle_max:
+                df.loc[ax:,trend_name] = trend
+                # tests
+                if type == "res":
+                    # print ('min id: ', df['val'].rolling(window=ticker_gap).max().idxmin(), 'val', df.iloc[df['val'].rolling(window=ticker_gap).max().idxmin()][trend_name])
+                    # print ('max id: ', df['val'].rolling(window=ticker_gap).max().idxmax(), 'val', df.iloc[df['val'].rolling(window=ticker_gap).max().idxmax()][trend_name])
+
+                    tests = df.loc[in_range(df[trend_name], df['val'], tolerance) &
+                    df['val'].rolling(window=ticker_gap).max() < \
+                    df.iloc[df['val'].rolling(window=ticker_gap).max().idxmax()][trend_name]]
+                    # print(tests)
+                    # print('RESISTANCE MIN confirmations: ',confirmations)
+                    # print('RESISTANCE found: ', len(tests))
+                    if len(tests) > confirmations:
+                        confirmed_idx = tests.index[confirmations-1]
+                        # trace trend from confirmation point
+                        # t_confirmed = df.index[confirmed_idx:]
+                        df.loc[:confirmed_idx,trend_name] = np.NaN
+
+                if type == "sup":
+                    # print ('min id: ', df['val'].rolling(window=ticker_gap).min().idxmin(), 'val', df.iloc[df['val'].rolling(window=ticker_gap).min().idxmin()][trend_name])
+                    # print ('max id: ', df['val'].rolling(window=ticker_gap).min().idxmax(), 'val', df.iloc[df['val'].rolling(window=ticker_gap).min().idxmax()][trend_name])
+                    tests = df.loc[in_range(df[trend_name], df['val'], tolerance) &
+                        df['val'].rolling(window=ticker_gap).min() > \
+                        df.iloc[df['val'].rolling(window=ticker_gap).min().idxmin()][trend_name]]
+                    # print(tests)
+                    # print('SUPPORT MIN confirmations: ',confirmations)
+                    # print('SUPPORT found: ', len(tests))
+                    if len(tests) > confirmations:
+                        confirmed_idx = tests.index[confirmations-1]
+                        # trace trend from confirmation point
+                        # t_confirmed = df.index[confirmed_idx:]
+                        df.loc[:confirmed_idx,trend_name] = np.NaN
+                        # slope, intercept, r_value, p_value, std_err = stats.linregress([ax, bx], [ay, by])
+                        # trend = polyval([slope,intercept],t_confirmed)
+                        # del df[trend_name]
+                        # df.loc[confirmed_idx:,trend_name] = trend
+            # cond = True
+
+
+    # trends_names = [c for c in df if c.startswith('trend|')]
+    trends_column_names = [c for c in df if c.startswith('trend|')]
+    if type == "res":
+        def set_res(row):
+            # print ('df.apply')
+            # for t in row[[c for c in df if c.startswith('trend')]]:
+
+            lower_res = 9999999999999
+            # print (row)
+            for t in trends_column_names:
+                if row[t] != 0 and (row[t] * (1+fake)) > row.val and row[t] < lower_res:
+                    lower_res = row[t]
+                    # print (lower_res)
+            if lower_res != 9999999999999:
+                # print ('higher', lower_res)
+                # row['trend'] = lower_res
+                return lower_res
+            else:
+                # print (df[:row.name].val.max())
+                return df[:row.name].val.max()
+        # mask = (df.val.shift(1) <= df.val) & (df.val.shift(-1) <= df.val)
+        # df['trend'] = df[mask].apply(set_res, axis=1)
+        # print ('count fractals: ', len(df.loc[(df.val.shift(1) <= df.val) & (df.val.shift(-1) <= df.val)]))
+        # df = df[(df.val.shift(1) <= df.val) & (df.val.shift(-1) <= df.val)].assign(trend=df.apply(set_res, axis=1))
+        df = df.assign(trend=df.apply(set_res, axis=1))
+
+    if type == "sup":
+        def set_sup(row):
+            # print ('df.apply')
+            # for t in row[[c for c in df if c.startswith('trend')]]:
+            higher_sup = 0
+            for t in trends_column_names:
+                if row[t] > 0 and (row[t] * (1-fake)) < row.val and row[t] > higher_sup:
+                    higher_sup = row[t]
+                    # print ('higher sup: ', higher_sup)
+            if higher_sup > 0:
+                # print ('higher', higher_sup)
+                # row['trend'] = higher_sup
+                return higher_sup
+            else:
+                # print (df[:row.name].val.min())
+                prev_low = df[:row.name].val.min()
+                if prev_low < row.val:
+                    return df[:row.name].val.min()
+                else:
+                    return row.val * (1-tolerance)
+
+        # mask = (df.val.shift(1) >= df.val) & (df.val.shift(-1) >= df.val)
+        # df['trend'] = df[mask].apply(set_sup, axis=1)
+        # df.trend.fillna(method='ffill')
+        df = df.assign(trend=df.apply(set_sup, axis=1))
+        # df = df[(df.val.shift(1) >= df.val) &
+        #         (df.val.shift(-1) >= df.val)].assign(trend=df.apply(set_sup, axis=1))
+        # df = df[(df.val.shift(2) <= df.val.shift(1)) &
+        #         (df.val.shift(1) <= df.val) &
+        #         (df.val.shift(-1) <= df.val) &
+        #         (df.val.shift(-2) <= df.val.shift(-1))].assign(trend=df.apply(set_sup, axis=1))
+        # df['trend'] = df[(df.val.shift(1) >= df.val) & (df.val.shift(-1) >= df.val)].apply(set_sup, axis=1)
+
+
+    # print (df)
+    trends = df[[c for c in df.columns if c.startswith('trend|')]]
+    # print (trends)
+    if chart == True:
+        # plot_pivots(df.close, df.low, df.high, pivots)
+        plot_pivots(df['val'], pivots_list, pair=pair, interval=interval)
+    # print (df['trend'])
+    return df['trend'], trends
+
+def get_trends_lightbuoy_OHCL(self, df, interval: int, chart=False):
 
     # volatility settings for threshold for peak and valleys
     volat_window = {
@@ -104,8 +383,10 @@ def gentrends(self, df, interval: int, chart=False):
     window = volat_window[interval]
     df['bb_exp'] = (df.bb_upperband.rolling(window=window).max() - df.bb_lowerband.rolling(window=window).min()) / df.bb_upperband.rolling(window=window).max()
 
-    pivots = peak_valley_pivots(df.low.values, df.high.values, df.bb_exp.values)
+    pivots = zigzag_hi_lo.peak_valley_pivots(df.low.values, df.high.values, df.bb_exp.values)
     df['pivots'] = np.transpose(np.array((pivots)))
+
+
 
     # separate high and low peaks
     h = df.loc[df['pivots']==1]
--- user_data/strategies/trend001.py
+++ user_data/strategies/trend001.py
@@ -4,12 +4,6 @@ import talib.abstract as ta
 from pandas import DataFrame
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from freqtrade.strategy.interface import IStrategy
-# from scripts import trendy
-from indicators import in_range, went_down, get_trend_lines, get_pivots
-# from freqtrade.persistence import Pair
-
-# from freqtrade.persistence import *
-
 from user_data.indicators.trendlines import *
 
 class_name = 'DefaultStrategy'
@@ -28,9 +22,11 @@ class trend001(IStrategy):
 
     # Minimal ROI designed for the strategy
     minimal_roi = {
-        "1200":  0.000,
-        "600":  0.04,
-        "0":  0.05
+        "120":  0.00001,
+        "80":  0.01,
+        "60":  0.001,
+        "30":  0.005,
+        "0":  0.01
     }
 
     # Optimal stoploss designed for the strategy
@@ -39,20 +35,46 @@ class trend001(IStrategy):
     # Optimal ticker interval for the strategy
     ticker_interval = "5m"
 
-    def populate_indicators(self, dataframe: DataFrame) -> DataFrame:
+    def populate_indicators(self, dataframe: DataFrame, pair: str) -> DataFrame:
 
         """
         Indicator for trends
         """
 
-        # Bollinger bands
-        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
-        dataframe['bb_lowerband'] = bollinger['lower']
-        dataframe['bb_middleband'] = bollinger['mid']
-        dataframe['bb_upperband'] = bollinger['upper']
 
         # dataframe = get_pivots(self, dataframe)
-        dataframe = gentrends(self, dataframe, self.ticker_interval, chart=True)
+        # print (len(dataframe), 'before get_trends')
+        # dataframe = get_trends(self, dataframe.high,
+        #             interval=self.ticker_interval,
+        #             type='res', tolerance=0.001, confirmations=3,
+        #             slope_max = 20,
+        #             slope_min = 95,
+        #             chart=False)
+
+        # dataframe['res_trend'] = get_trends(self, dataframe.high,
+        #             interval=self.ticker_interval,
+        #             type='res', tolerance=0.001, confirmations=3,
+        #             angle_max = 20, angle_min = 95,
+        #             thresh_up = 0.01, thresh_down = -0.01,
+        #             chart=True)
+        dataframe['res_trend'], res_trends = get_trends_serie(self, dataframe.high,
+                    interval=self.ticker_interval,
+                    type='res', tolerance=0.0001, confirmations=2,
+                    angle_max = 90,
+                    angle_min = -70,
+                    thresh_up = 0.02, thresh_down = -0.02,
+                    chart=False)
+        dataframe['sup_trend'], sup_trends = get_trends_serie(self, dataframe.low,
+                    interval=self.ticker_interval,
+                    type='sup', tolerance=0.0001, confirmations=3,
+                    angle_max = 180,
+                    angle_min = 0,
+                    thresh_up = 0.02, thresh_down = -0.02,
+                    chart=False)
+
+
+        # print (dataframe.sup_trend)
+        # plot_trends_new(dataframe, interval=self.ticker_interval)
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame) -> DataFrame:
@@ -64,7 +86,8 @@ class trend001(IStrategy):
 
         dataframe.loc[
             (
-                (in_range(dataframe['close'],dataframe['trendline-min']*1.001, 0.001))
+                (in_range(dataframe['close'],dataframe.sup_trend*1.001, 0.001))
+                # (dataframe['close']==dataframe['sup_trend'])
             ),
             'buy'] = 1
         return dataframe
@@ -77,10 +100,10 @@ class trend001(IStrategy):
         """
         dataframe.loc[
             (
-                (dataframe['close'] >= dataframe['trendline-max']*0.99)
-                |
-                (dataframe['close'] <= dataframe['trendline-min'] * 0.95)
-
+                # (dataframe['close'] >= dataframe['res_trend'].shift(2))
+                # |
+                # (dataframe['close'] <= dataframe['sup_trend'] * 0.95)
+                0
             ),
             'sell'] = 1
         # print (dataframe.loc[dataframe['sell']==1].close)
