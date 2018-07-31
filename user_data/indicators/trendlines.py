d28a8e1c9201e0e8b42e3e2d737c54b8121b080f
Bruno Sarlo <bsarlo@gmail.com>
2018-07-31 02:19:22 -0300

    current status, WIP yet. Will move to work with dictionaries first thing next.

--- user_data/indicators/trendlines.py
+++ user_data/indicators/trendlines.py
@@ -21,6 +21,7 @@ pyximport.install(reload_support=True)
 
 
 
+
 def get_trends_OHCL(df: DataFrame, interval: int, tolerance: int, pivot_type: str, \
                      su_min_tests: int, re_min_tests: int, body_min_tests: int, \
                      ticker_gap: int, fake: int, nearby: int, angle_min: int, \
@@ -31,9 +32,14 @@ def get_trends_OHCL(df: DataFrame, interval: int, tolerance: int, pivot_type: st
     start_time = time.time()
     print('finding trends by concurrence')
 
-#     p = df.loc[df['pivots']==-1]
+    if pivot_type == 'pivots':
+        df, pivots = get_pivots_OHCL(df, period=20, stdv= 1.2,
+                    interval=interval, thresh_type = 'dynamic',
+                    thresh_up = 0.05, thresh_down = -0.05,
+                    chart=False, pair=pair)
+    elif pivot_type == 'fractals':
+        df = get_fractals(df)
 
-    df = get_fractals(df)
     p = df.loc[df[pivot_type]==-1]
 
     for i in range(0, len(p)-1):
@@ -93,44 +99,50 @@ def get_trends_lightbuoy_OHCL(df: DataFrame, interval: int, tolerance: int, pivo
     start_time = time.time()
     print('finding trends - lightbuoy style')
 
+    if pivot_type == 'pivots':
+        df, pivots = get_pivots_OHCL(df, period=20, stdv= 1.2,
+                    interval=interval, thresh_type = 'dynamic',
+                    thresh_up = 0.03, thresh_down = -0.03,
+                    chart=False, pair=pair)
+    elif pivot_type == 'fractals':
+        df = get_fractals(df)
 
-    df = get_fractals(df)
+    # df = get_fractals(df)
 
+    trends = []
 
     r = df.loc[df[pivot_type]==1]
     rx = r.index[0]
     ry = r.iloc[0].high
-    df.loc[0:, 'r1_trend'] = ry
+    df.loc[0:r.index[1], 'r1_trend'] = ry
 
     s = df.loc[df[pivot_type]==-1]
 
-    # just working with supports now.
+    # just working with supports now
     p = s
     last=len(p)-3
     i=0
+    ai = i
+    df.loc[0:p.index[1],'s1_trend'] = p.iloc[i].low
 
     prev_ai = []
-    ai = i
-    ax = p.index[i]
-    ay = p.iloc[i].low
-    df.loc[0:,'s1_trend'] = ay
 
     b=True
-    bx = p.index[i+1]
-    by = p.iloc[i+1].low
 
     while b:
+        ax = p.index[i]
+        ay = p.iloc[i].low
+
+        bx = p.index[i+1]
+        by = p.iloc[i+1].low
 
         # trace first trend
-
         t = df.index[ax:]
         slope, intercept, r_value, p_value, std_err = stats.linregress([ax, bx], [ay, by])
         trend = polyval([slope,intercept],t)
         angle = calculate_angle(ax, ay, bx, by)
         trend_name = 'trend|'+str(ax)+'|'+str(ay)+'|'+str(bx)+'|'+str(by)+'|'+str(round(angle))
         df.loc[ax:,trend_name] = trend
-        df.loc[ax:,'s1_trend'] = trend
-
 
         df = get_line_tests(df, trend_name, pivot_type, nearby, fake)
         data_name = 'data_'+ trend_name
@@ -139,38 +151,62 @@ def get_trends_lightbuoy_OHCL(df: DataFrame, interval: int, tolerance: int, pivo
         su_name = data_name+'|su'
         body_name = data_name+'|body'
 
-        cx = p.index[i+2]
-        cy = p.iloc[i+2].low
+        trend = {'name':trend_name,
+                'interval':interval,
+                'a':[ax, ay, df.iloc[ax].date],
+                'b':[bx, by, df.iloc[bx].date],
+                'linregress'
+                'angle':angle,
+                'su_conf':np.count_nonzero(df[su_name]), # just so we know last totals
+                're_conf':np.count_nonzero(df[re_name]),
+                'body_conf':np.count_nonzero(df[body_name]),
+                'type':'res',
+                'last':False,
+                'max':False,
+                'min':False}
+        trends.append(trend)
 
+        # check indicator parameters
         cond = angle >= angle_min and angle <= angle_max \
                 and np.count_nonzero(df[su_name]) >= su_min_tests
 
-        if cy <= (df.loc[cx][trend_name] * (1-fake)):
+        # set it as support if valid
+        if cond == True and (df.iloc[bx][trend_name]) <= df.iloc[bx]['low']:
+            df.loc[bx:,'s1_trend'] = df.loc[bx:,trend_name]
+
+        # next point
+        nx = p.index[i+2]
+        ny = p.iloc[i+2].low
+
+        if ny <= (df.loc[nx][trend_name]):
             # we can use this support broken as resistance but better do it after a confirmation
-            if cond == True and (df.iloc[cx][trend_name] * (1-fake)) <= df.iloc[cx]['r1_trend']:
-                df.loc[cx:,'r1_trend'] = df.loc[cx:,trend_name]
+            # still need to draw the line from the confirmation point, right now from third point nx
+            if cond == True and (df.iloc[nx][trend_name]) >= df.iloc[nx]['high'] and angle < 90:
+                df.loc[bx:,'r1_trend'] = df.loc[bx:,trend_name]
             if len(prev_ai)>0:
                 ai = prev_ai.pop()
                 ax = p.index[ai]
                 ay = p.iloc[ai].low
-            bx = cx
-            by = cy
-        elif cy > (df.loc[cx][trend_name] * (1+nearby)):
-            if cond == True and (df.iloc[cx][trend_name] * (1+nearby)) >= df.iloc[cx]['s1_trend']:
-                df.loc[bx:,'s1_trend'] = df.loc[cx:,trend_name]
-            prev_ai.append(ai)
+            else:
+                ai = i
+                ax = p.index[ai]
+                ay = p.iloc[ai].low
+            bx = nx
+            by = ny
+        elif ny > (df.loc[nx][trend_name]):
+            if cond == True and (df.iloc[nx][trend_name]) <= df.iloc[nx]['low'] and angle > 90:
+                df.loc[nx:,'s1_trend'] = df.loc[nx:,trend_name]
             ax = bx
             ay = by
-            ai = i
-            bx = cx
-            by = cy
+            prev_ai.append(i)
+            bx = nx
+            by = ny
         else:
-            bx = cx
-            by = cy
+            bx = nx
+            by = ny
 
         if i == last:
             b = False
         i += 1
-
     print("--- %s seconds ---" % (time.time() - start_time))
     return df
--- user_data/strategies/trendOHCL001.py
+++ user_data/strategies/trendOHCL001.py
@@ -31,10 +31,10 @@ class trendOHCL001(IStrategy):
     }
 
     # Optimal stoploss designed for the strategy
-    stoploss = -0.01
+    stoploss = -0.03
 
     # Optimal ticker interval for the strategy
-    ticker_interval = "5m"
+    ticker_interval = "1m"
 
     def populate_indicators(self, dataframe: DataFrame, pair: str) -> DataFrame:
 
@@ -85,9 +85,9 @@ class trendOHCL001(IStrategy):
         # dataframe['cmf'] = cmf(dataframe)
 
         dataframe = get_trends_lightbuoy_OHCL(dataframe,
-            interval=self.ticker_interval, pivot_type='fractals',
-            tolerance=0.00001, su_min_tests=3, re_min_tests=5, body_min_tests=1, ticker_gap = 5, fake=0.00001, nearby=0.00001,
-            angle_max = 85, angle_min = 75,
+            interval=self.ticker_interval, pivot_type='pivots',
+            tolerance=0.00001, su_min_tests=4, re_min_tests=2, body_min_tests=1, ticker_gap = 5, fake=0.0001, nearby=0.001,
+            angle_max = 100, angle_min = 80,
             thresh_up = 0.01, thresh_down = -0.01,
             chart=False, pair=pair)
 
@@ -104,7 +104,7 @@ class trendOHCL001(IStrategy):
 
         return dataframe
 
-    def populate_buy_trend(self, dataframe: DataFrame) -> DataFrame:
+    def populate_buy_trend(self, dataframe: DataFrame, pair: str) -> DataFrame:
         """
         Based on TA indicators, populates the buy signal for the given dataframe
         :param dataframe: DataFrame
@@ -113,20 +113,25 @@ class trendOHCL001(IStrategy):
 
         dataframe.loc[
             (
-                (dataframe['close'] <= dataframe.s1_trend)
+                (dataframe['close'] == dataframe.s1_trend)
                 # |
                 # (dataframe['close'] > dataframe.re_trend)
                 # (dataframe['close'] <= dataframe.sup_trend)
                 &
-                (dataframe['volume'].shift(1) < dataframe['volume']/3)
-                # &
-                # (dataframe['rsi'] < 25)
+                (dataframe['volume'].shift(1) < dataframe['volume']/10)
+                &
+                (dataframe['rsi'] < 25)
                 &
                 (dataframe.r1_trend >= dataframe.s1_trend*1.03)
                 # (dataframe['close']==dataframe['sup_trend'])
                 # 0
             ),
             'buy'] = 1
+
+
+        # UNCOMMENT TO PLOT
+        # self.plot_dataframe(dataframe, pair, ['s1_trend,r1_trend', '', 'rsi'])
+
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame) -> DataFrame:
@@ -139,7 +144,7 @@ class trendOHCL001(IStrategy):
             (
                 (dataframe['close'] >= dataframe['r1_trend'])
                 |
-                (dataframe['close'] <= dataframe['s1_trend'] * 0.98)
+                (dataframe['close'] <= dataframe['s1_trend'] * 0.998)
                 # |
                 # (dataframe['close'] <= dataframe['sup_trend'] * (1 - dataframe.iloc[-1].bb_exp))
                 # 0
@@ -147,3 +152,103 @@ class trendOHCL001(IStrategy):
             'sell'] = 1
 
         return dataframe
+
+
+    def plot_dataframe(self, data, pair, indicators: list):
+        """
+        plots our dataframe everytime new data arrive
+        and a tick is closed
+        :param indicators: list of indicators
+        :param data:
+        :return:
+        """
+
+        import plotly.graph_objs as go
+        from plotly import tools
+        from plotly.offline import plot as plt
+
+        def generate_row(fig, row, raw_indicators, data) -> tools.make_subplots:
+            """
+            Generator all the indicator selected by the user for a specific row
+            """
+            if raw_indicators is None or raw_indicators == "":
+                return fig
+            for indicator in raw_indicators.split(','):
+                if indicator in data:
+                    scattergl = go.Scattergl(
+                        x=data['date'],
+                        y=data[indicator],
+                        name=indicator
+                    )
+                fig.append_trace(scattergl, row, 1)
+
+
+            return fig
+
+        rows = len(indicators)
+
+        if rows < 3:
+            rows = 3
+
+        # Define the graph
+        fig = tools.make_subplots(
+            rows=rows,
+            cols=1,
+            shared_xaxes=True,
+            row_width=[1, 1, 4],
+            vertical_spacing=0.0001,
+        )
+
+        fig['layout'].update(title=pair)
+        fig['layout']['yaxis1'].update(title='Price')
+        fig['layout']['yaxis2'].update(title='Volume')
+        fig['layout']['yaxis3'].update(title='Other')
+
+        if rows > 3:
+            for x in range(3, rows):
+                fig['layout']['yaxis{}'.format(x)].update(title='Other {}'.format(x))
+
+        # Common information
+        candles = go.Candlestick(
+            x=data.date,
+            open=data.open,
+            high=data.high,
+            low=data.low,
+            close=data.close,
+            name='Price'
+        )
+        fig.append_trace(candles, 1, 1)
+
+        df_buy = data[data['buy'] == 1]
+        buys = go.Scattergl(
+            x=df_buy.date,
+            y=df_buy.close,
+            mode='markers',
+            name='buy',
+            marker=dict(
+                symbol='triangle-up-dot',
+                size=9,
+                line=dict(width=1),
+                color='green',
+            )
+        )
+
+        fig.append_trace(buys, 1, 1)
+        # Row 2
+        volume = go.Bar(
+            x=data['date'],
+            y=data['volume'],
+            name='Volume'
+        )
+        fig.append_trace(volume, 2, 1)
+
+        row = 0
+        for indicator in indicators:
+            row = row + 1
+            # print(row)
+            generate_row(fig, row, indicator, data)
+
+        from pathlib import Path
+        plt(fig, auto_open=False, filename=str(
+            Path('user_data').joinpath(
+                "{}_{}_analyze_{}_{}.html".format(__class__.__name__, pair.replace('/', '-'), self.ticker_interval, data['date'].iloc[-1]))))
